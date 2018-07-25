be5a224467cc027966c09e2d2fbe3299ee58a7c9
Lakshman Pavan Kumar Boddoju <pavanboddoju@gmail.com>
2018-07-25 13:31:55 -0500

    changes

--- model_pytorch.py
+++ model_pytorch.py
@@ -201,7 +201,7 @@ class MultipleChoiceHead(nn.Module):
         nn.init.normal_(self.linear.weight, std = 0.02)
         nn.init.normal_(self.linear.bias, 0)
 
-    def forward(self, h, x):
+    def forward(self, h, x, ELMo):
         # Classification logits
         clf_h = h.view(-1, self.n_embd)
         flat = x[..., 0].contiguous().view(-1)
@@ -213,6 +213,15 @@ class MultipleChoiceHead(nn.Module):
         # https://github.com/huggingface/pytorch-openai-transformer-lm/issues/11
         clf_h = self.dropout(clf_h.transpose(1, 2)).transpose(1, 2)
         clf_h = clf_h.contiguous().view(-1, self.n_embd)
+
+        elmo_a = ELMo[:, 0, :] + ELMo[:, 1, :]
+        elmo_b = ELMo[:, 0, :] + ELMo[:, 2, :]
+
+        elmo_comb = np.concatenate([elmo_a, elmo_b], axis=0)
+        elmo_comb = torch.from_numpy(elmo_comb)
+        elmo_comb = elmo_comb.view(-1, 2, 1024)
+
+        clf_h = elmo_comb
         clf_logits = self.linear(clf_h)
 
         return clf_logits.view(-1, x.size(1))
@@ -292,10 +301,10 @@ class DoubleHeadModel(nn.Module):
                              "'similarity', 'inference' or ('classification', n_class) "
                              f"got {task_head_type}.")
 
-    def forward(self, x):
+    def forward(self, x, ELMo):
         h = self.transformer(x)
         lm_logits = self.lm_head(h)
-        task_logits = self.task_head(h, x)
+        task_logits = self.task_head(h, x, ELMo)
 
         return lm_logits, task_logits
 
